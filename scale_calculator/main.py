ff91ea3127b0648661ce37cea2795b2803d9b18b
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-09-07 03:22:22 +0400

    Отрисовка скелета нотации

--- scale_calculator/main.py
+++ scale_calculator/main.py
@@ -33,14 +33,38 @@ def main(
     #     ..., help="Количество струн на гитаре. Например, 6."),
     tuning: str = typer.Option(
         ..., help="Гитарная настройка. Например, e_standard."),
-    scale: str = typer.Option(
-        ..., help="Музыкальная шкала. Например, minor."),
-    root: str = typer.Option(
-         ..., help="Корневая нота. Например, e."),
-    # neck_range: str = typer.Option(
-    #     ..., help="Диапазон грифа. Например, ноты E2-E4 или лады 1-12."),
+    # scale: str = typer.Option(
+    #     ..., help="Музыкальная шкала. Например, minor."),
+    # root: str = typer.Option(
+    #      ..., help="Корневая нота. Например, e."),
+    # fretboard: str = typer.Option(
+    #     ..., help="Диапазон грифа. Например, лады 0-5.")
+    num_frets: int = typer.Option(
+        ..., help="Количество ладов. Например, 6"),
 ):
-    typer.echo(f"Вы выбрали {tuning}, {scale}, {root}")
+    typer.echo(f"Вы выбрали {tuning}, {num_frets}")
+
+    if tuning == "e_standart" and num_frets == 6:
+        notation = draw_notation(["E4", "B3", "G3", "D3", "A2", "E2"], 6)
+        print(notation)
+
+
+def draw_notation(string_notes, num_frets):
+
+    num_hyphens = num_frets * 3
+
+    # верхняя горизонтальная линия
+    notation = "==|==" + "==".join([str(i) for i in range(num_frets)])
+
+    # гриф гитары
+    for note in string_notes:
+        notation += f"\n{note}|"
+        notation += "-" * num_hyphens
+
+    # нижняя горизонтальная линия
+    notation += "\n" + "==|==" + "==".join([str(i) for i in range(num_frets)])
+
+    return notation
 
 
 if __name__ == "__main__":
