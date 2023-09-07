a6c26a214181337179c56bdd3b562e8cf075b7f4
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-09-07 04:15:53 +0400

    Отрисовка скелета табулатуры

--- scale_calculator/main.py
+++ scale_calculator/main.py
@@ -1,9 +1,5 @@
 import typer
 
-
-# def main():
-#     print(
-#         """
 # ==|==0==1==2==3==4==5
 # E4|------------------
 # B3|------------------
@@ -12,19 +8,14 @@ import typer
 # A2|--4-----5--6------
 # E2|--1-----2--3------
 # ==|==0==1==2==3==4==5
-# """
-#     )
 
-#     print(
-#         """
 # e|-0-------
 # B|-0-1---3-
 # G|-0-2-----
 # D|-0-2---4-
 # A|-0-2-3---
 # E|-0-2-3---
-# """
-#     )
+
 
 def main(
     # guitar_type: str = typer.Option(
@@ -37,16 +28,20 @@ def main(
     #     ..., help="Музыкальная шкала. Например, minor."),
     # root: str = typer.Option(
     #      ..., help="Корневая нота. Например, e."),
-    # fretboard: str = typer.Option(
+    # fretboard: list = typer.Option(
     #     ..., help="Диапазон грифа. Например, лады 0-5.")
     num_frets: int = typer.Option(
         ..., help="Количество ладов. Например, 6"),
 ):
-    typer.echo(f"Вы выбрали {tuning}, {num_frets}")
+    typer.echo(f"Вы выбрали {tuning} и {num_frets} ладов!")
 
-    if tuning == "e_standart" and num_frets == 6:
-        notation = draw_notation(["E2", "A2", "D3", "G3", "B3", "E4"], 6)
-        print(notation)
+    if tuning == "e_standart":
+        string_notes = ["E2", "A2", "D3", "G3", "B3", "E4"]
+
+    print("Нотация:")
+    print(draw_notation(string_notes, num_frets))
+    print("Табулатура:")
+    print(draw_tablature(string_notes, num_frets))
 
 
 def draw_notation(string_notes, num_frets):
@@ -65,6 +60,17 @@ def draw_notation(string_notes, num_frets):
     notation += "\n" + "==|==" + "==".join([str(i) for i in range(num_frets)])
 
     return notation
+
+
+def draw_tablature(string_notes, num_frets):
+    tablature = f"{string_notes[0][0]}|"
+    tablature += "-" * 9
+
+    for note in string_notes[1:]:
+        tablature += f"\n{note[0]}|"
+        tablature += "-" * 9
+
+    return tablature
 
 
 if __name__ == "__main__":
