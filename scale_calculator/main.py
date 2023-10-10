cbb77f3b2729c05dd69ef3b5b82eb790b19e083b
Ольга Ротбардт <knyajna.tarakanova@gmail.com>
2023-10-10 20:52:30 +0400

    Правка шаблона

--- scale_calculator/main.py
+++ scale_calculator/main.py
@@ -6,8 +6,8 @@ import typer
 # G3|------------------
 # D3|--7-----1---------
 # A2|--4-----5--6-----7
-# E2|--1-----2--3-----4-----5--6-----7
-# ==|==0==1==2==3==4==5==6==7==8==9==10
+# E2|--1-----2--3-----4-----5--6-----7-----1-----2--3-----4-----5--6-----7------1
+# ==|==0==1==2==3==4==5==6==7==8==9==10=11=12=13=14=15=16=17=18=19=20=21=22=23=24
 
 """
 C-D-E-F-G-A-B-C
@@ -37,6 +37,7 @@ def main(
         ..., help="Музыкальная шкала. Например, minor."),
     root: str = typer.Option(
          ..., help="Корневая нота. Например, E."),
+    # открытая струна или закрытые струны,
     # fretboard: list = typer.Option(
     #     ..., help="Диапазон грифа. Например, лады 0-5.")
     num_frets: int = typer.Option(
@@ -44,10 +45,9 @@ def main(
 ):
     typer.echo(f"Вы выбрали: {tuning}, {scale}, {root} и {num_frets} ладов!")
 
-    if tuning == "e_standart":
+    # корневая нота на открытой струне
+    if tuning == "e_standart" and scale == "minor":
         string_notes = ["E2", "A2", "D3", "G3", "B3", "E4"]
-
-    if scale == "minor":
         string_position = "--1-----2--3-----4-----5--6-----7"
 
     print("Нотация:")
@@ -76,11 +76,28 @@ def draw_notation(string_notes, string_position, num_frets, root):
         if line.startswith(root):
             index_to_replace = len(lines) - 1 - index
             break
+# ---
 
+# 1
     # положения первой струны
     new_value = f"{note}|" + string_position[:num_hyphens]
     lines[index_to_replace] = new_value
 
+    # положения второй струны
+    new_value = f"{note}|" + string_position[num_hyphens+3:num_hyphens+3+num_hyphens]
+    lines[index_to_replace-1] = new_value
+
+    # положения третьей струны
+    new_value = f"{note}|" + string_position[num_hyphens+3+num_hyphens+3:] + "-" * 9
+    lines[index_to_replace-2] = new_value
+
+# 2
+    # for i in range(3):
+    #     current_note = string_notes[i]
+    #     new_value = f"{current_note}|" + string_position[i * (num_hyphens + 3):(i + 1) * (num_hyphens + 3)]
+    #     lines[index_to_replace - i] = new_value
+
+# --
     # гриф гитары
     notation += "\n" + "\n".join(lines)
 
